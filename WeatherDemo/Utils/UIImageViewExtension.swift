b3ef254ec316f45fffedc29f2e19815fa6667b63
Anis ur Rehman <anis@kinnov.com>
2023-02-16 17:56:14 +0500

    center align activity indicator

--- WeatherDemo/Utils/UIImageViewExtension.swift
+++ WeatherDemo/Utils/UIImageViewExtension.swift
@@ -11,8 +11,15 @@ import UIKit
 extension UIImageView {    
     func setImage(from urlString: String, imageLoader: URLImageLoading) {
         Task {
+            let activityIndicatorView = UIActivityIndicatorView()
+            activityIndicatorView.startAnimating()
+            self.addSubview(activityIndicatorView)
+            debugPrint(self.frame)
+            let center = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds));
+            activityIndicatorView.center = center
             let image = await imageLoader.getImage(from: urlString)
             self.image = image
+            activityIndicatorView.removeFromSuperview()
         }
     }
 }
--- WeatherDemoTests/WeatherServiceTests.swift
+++ WeatherDemoTests/WeatherServiceTests.swift
@@ -13,12 +13,12 @@ final class WeatherServiceTests: XCTestCase {
     func test_Fetch_Weather_Success() async throws {
         let coord = WeatherResponse.Coord(lat: 30.1956, lon: 71.4753)
         let main = WeatherResponse.Main(temp: 297.09, temp_min: 297.09, temp_max: 297.09)
-        let service = WeatherService(client: MockAPIClient(returnsSuccess: true, main: main, coord: coord, weather: nil))
+        let weather = WeatherResponse.Weather(id: 0, main: "", description: "", icon: "")
+        let service = WeatherService(client: MockAPIClient(returnsSuccess: true, main: main, coord: coord, weather: [weather]))
         let cityWeatherList = await service.getWeather(cityNameList: ["Multan"])
-        if cityWeatherList.count > 0 {
-            let weather = cityWeatherList[0]
-            XCTAssertEqual(weather.city, "Multan")
-        }
+        XCTAssertEqual(cityWeatherList.count, 1)
+        let weather1 = cityWeatherList[0]
+        XCTAssertEqual(weather1.city, "Multan")
     }
 
     func test_Fetch_Weather_Failed() async throws {
