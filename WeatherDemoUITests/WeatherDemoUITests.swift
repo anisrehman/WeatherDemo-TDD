5b7d291b34e95fcdca83d3cc87dc2a5374ec3a66
Anis ur Rehman <anis.rehman@appinsnap.com>
2023-07-05 15:31:11 +0500

    UI test working

--- WeatherDemoUITests/WeatherDemoUITests.swift
+++ WeatherDemoUITests/WeatherDemoUITests.swift
@@ -27,15 +27,21 @@ final class WeatherDemoUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        sleep(1) // Let api get called
+        let multanCell = app.tables.cells.containing(NSPredicate(format: "label CONTAINS %@", "Multan")).firstMatch
+        XCTAssertTrue(multanCell.exists)
+        multanCell.tap()
+        
+        var navigationBar = app.navigationBars.containing(NSPredicate(format: "label CONTAINS %@", "Multan")).firstMatch
+        XCTAssertTrue(navigationBar.exists)
+        
+        app.navigationBars.buttons.firstMatch.tap()
+        
+        let karachiCell = app.tables.cells.containing(NSPredicate(format: "label CONTAINS %@", "Karachi")).firstMatch
+        XCTAssertTrue(karachiCell.exists)
+        karachiCell.tap()
+        
+        navigationBar = app.navigationBars.containing(NSPredicate(format: "label CONTAINS %@", "Karachi")).firstMatch
+        XCTAssertTrue(navigationBar.exists)
     }
-
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
 }
